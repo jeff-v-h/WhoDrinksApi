9fb2ffeb599702bce4126ca2931620cbc2f2caf4
jeffrey-huang-recaura <jeffreyhuang@recaura.com>
2021-02-25 21:26:02 +1100

    create initial user and usercredentials models

---
+++ DontThinkJustDrink.Api/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace DontThinkJustDrink.Api.Models
+{
+    public class User
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public bool AgreedToPrivacyPolicy { get; set; }
+        public bool AgreedToTermsAndConditions { get; set; }
+    }
+}
---
+++ DontThinkJustDrink.Api/Models/UserCredentials.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace DontThinkJustDrink.Api.Models
+{
+    public class UserCredentials
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        public string UserId { get; set; }
+        public string Username { get; set; }
+        public string Salt { get; set; }
+        public string Hashed { get; set; }
+    }
+}
