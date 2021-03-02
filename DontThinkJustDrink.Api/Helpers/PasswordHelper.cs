3d6c3d8006f8c4d38cea1e13e4ae73ec1a64a649
jeffrey-huang-recaura <jeffreyhuang@recaura.com>
2021-03-02 22:14:00 +1100

    add initial method to check password

--- DontThinkJustDrink.Api/Helpers/PasswordHelper.cs
+++ DontThinkJustDrink.Api/Helpers/PasswordHelper.cs
@@ -6,12 +6,14 @@ namespace DontThinkJustDrink.Api.Helpers
 {
     public class PasswordHelper : IPasswordHelper
     {
+        private const int Iterations = 10000;
+
         public string Hash(string pw)
         {
             var saltAsBytes = GenerateRandomSaltBytes();
             var salt = Convert.ToBase64String(saltAsBytes);
-            var hashed = DeriveHash(pw, saltAsBytes, 10000);
-            return $"{salt.Substring(0, 8)}{hashed}.{salt.Substring(8)}";
+            var hashed = DeriveHash(pw, saltAsBytes, Iterations);
+            return $"{salt.Substring(0, 8)}{hashed}.{salt.Substring(8)}.{Iterations}";
         }
 
         private byte[] GenerateRandomSaltBytes()
@@ -33,6 +35,28 @@ namespace DontThinkJustDrink.Api.Helpers
                 prf: KeyDerivationPrf.HMACSHA1,
                 iterationCount: iterations,
                 numBytesRequested: 256 / 8));
+        }
+
+        public (bool verified, bool needsUpgrade) Check(string hash, string password)
+        {
+            var parts = hash.Split('.', 3);
+
+            if (parts.Length != 3) {
+                throw new FormatException("Unexpected hash format.");
+            }
+
+            var salt = parts[0].Substring(0, 8) + parts[1];
+            var saltAsBytes = Convert.FromBase64String(salt);
+            var hashed = DeriveHash(password, saltAsBytes, Iterations);
+
+            if (hashed != hash) {
+                return (false, false);
+            }
+
+            var iterations = Convert.ToInt32(parts[2]);
+            var needsUpgrade = iterations < 100;
+
+            return (true, needsUpgrade);
         }
     }
 }
