c2c3232d367bf68eabeff4671d9dcade267946e7
jeffrey-huang-recaura <jeffreyhuang@recaura.com>
2021-03-02 22:13:41 +1100

    add initial login request and response models

---
+++ DontThinkJustDrink.Api/Models/RequestModels/LoginRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace DontThinkJustDrink.Api.Models.RequestModels
+{
+    public class LoginRequest
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ DontThinkJustDrink.Api/Models/ResponseModels/LoginResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace DontThinkJustDrink.Api.Models.ResponseModels
+{
+    public class LoginResponse
+    {
+        public string Username { get; set; }
+        public bool ShouldUpdatePassword { get; set; }
+    }
+}
